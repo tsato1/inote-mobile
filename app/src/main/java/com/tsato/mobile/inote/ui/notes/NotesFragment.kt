1cc7723fa11602e4949235600944dfcf9009b278
tsato1 <a8tb1117@gmail.com>
2021-09-29 15:20:23 +0800

    view and viewmodel linked

--- app/src/main/java/com/tsato/mobile/inote/ui/notes/NotesFragment.kt
+++ app/src/main/java/com/tsato/mobile/inote/ui/notes/NotesFragment.kt
@@ -1,16 +1,21 @@
 package com.tsato.mobile.inote.ui.notes
 
 import android.content.SharedPreferences
+import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_USER
 import android.os.Bundle
 import android.view.*
+import androidx.fragment.app.viewModels
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tsato.mobile.inote.R
+import com.tsato.mobile.inote.adapters.NoteAdapter
 import com.tsato.mobile.inote.ui.BaseFragment
 import com.tsato.mobile.inote.util.Constants.KEY_LOGGED_IN_EMAIL
 import com.tsato.mobile.inote.util.Constants.KEY_PASSWORD
 import com.tsato.mobile.inote.util.Constants.NO_EMAIL
 import com.tsato.mobile.inote.util.Constants.NO_PASSWORD
+import com.tsato.mobile.inote.util.Status
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_notes.*
 import javax.inject.Inject
@@ -18,8 +23,12 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class NotesFragment : BaseFragment(R.layout.fragment_notes) {
 
+    private val viewModel: NotesViewModel by viewModels()
+
     @Inject
     lateinit var sharedPref: SharedPreferences
+
+    private lateinit var noteAdapter: NoteAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,10 +41,58 @@ class NotesFragment : BaseFragment(R.layout.fragment_notes) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        requireActivity().requestedOrientation = SCREEN_ORIENTATION_USER // the orientation user chose
+
+        setupRecyclerView()
+        subscribeToObservers()
+
+        noteAdapter.setOnItemClickListener {
+            findNavController().navigate(
+                NotesFragmentDirections.actionNotesFragmentToNoteDetailFragment(it.id)
+            )
+        }
 
         fabAddNote.setOnClickListener {
             findNavController().navigate(NotesFragmentDirections.actionNotesFragmentToAddEditNoteFragment(""))
         }
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.allNotes.observe(viewLifecycleOwner, {
+            it?.let { event -> // we made event because we don't want to show snackbar twice on configuration change
+                val result = event.peekContent()
+
+                when (result.status) {
+                    Status.SUCCESS -> {
+                        noteAdapter.notes = result.data!! // not null because it's success
+                        swipeRefreshLayout.isRefreshing = false
+                    }
+                    Status.ERROR -> { // consume the event content only in error, only on the first time
+                        event.getContentIfNotHandled()?.let { errorResource ->
+                            errorResource.message?.let { message ->
+                                showSnackbar(message)
+                            }
+                        }
+                        result.data?.let { notes ->
+                            noteAdapter.notes = notes
+                        }
+                        swipeRefreshLayout.isRefreshing = false
+                    }
+                    Status.LOADING -> {
+                        result.data?.let { notes ->
+                            noteAdapter.notes = notes
+                        }
+                        swipeRefreshLayout.isRefreshing = true
+                    }
+                }
+            }
+        })
+    }
+
+    private fun setupRecyclerView() = rvNotes.apply {
+        noteAdapter = NoteAdapter()
+        adapter = noteAdapter
+        layoutManager = LinearLayoutManager(requireContext())
     }
 
     private fun logout() {
--- app/src/main/java/com/tsato/mobile/inote/ui/notes/NotesViewModel.kt
+++ app/src/main/java/com/tsato/mobile/inote/ui/notes/NotesViewModel.kt
@@ -1,6 +1,24 @@
 package com.tsato.mobile.inote.ui.notes
 
-import androidx.lifecycle.ViewModel
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.*
+import com.tsato.mobile.inote.data.local.entities.Note
+import com.tsato.mobile.inote.repositories.NoteRepository
+import com.tsato.mobile.inote.util.Event
+import com.tsato.mobile.inote.util.Resource
 
-class NotesViewModel : ViewModel() {
+class NotesViewModel @ViewModelInject constructor(
+    private val repository: NoteRepository
+): ViewModel() {
+
+    private val _forceUpdate = MutableLiveData<Boolean>(false)
+
+    private val _allNotes = _forceUpdate.switchMap {
+        repository.getAllNotes().asLiveData(viewModelScope.coroutineContext)
+    }.switchMap {
+        MutableLiveData(Event(it)) // wrap the data(it) that we got from database around Event
+    }
+    val allNotes: LiveData<Event<Resource<List<Note>>>> = _allNotes
+
+    fun syncAllNotes() = _forceUpdate.postValue(true)
 }
